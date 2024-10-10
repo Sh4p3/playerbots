19d957e2621d287d47424d2ca68e162e706e16c4
Unknown <unknown@example.com>
2024-10-10 14:53:06 +0200

    arena bots must be 80 instead of 70+

--- playerbot/RandomPlayerbotFactory.cpp
+++ playerbot/RandomPlayerbotFactory.cpp
@@ -926,7 +926,7 @@ void RandomPlayerbotFactory::CreateRandomArenaTeams()
         Player* player = sObjectMgr.GetPlayer(captain);
         if (player)
         {
-            if (player->GetLevel() < 70)
+            if (player->GetLevel() < 80)
                 continue;
 
             uint8 slot = ArenaTeam::GetSlotByType(ArenaType(ARENA_TYPE_2v2));
@@ -984,9 +984,9 @@ void RandomPlayerbotFactory::CreateRandomArenaTeams()
             continue;
         }
 
-        if (player->GetLevel() < 70)
+        if (player->GetLevel() < 80)
         {
-            sLog.outError("Bot %d must be level 70 to create an arena team", (uint64)captain);
+            sLog.outError("Bot %d must be level 80 to create an arena team", (uint64)captain);
             continue;
         }
 
