a45b508da288e136ea7f3a9fd05ef0433f72829f
Sh4p3 <gheysens@hotmail.fr>
2026-03-04 17:43:23 +0100

    Use Tidal Force before Chain Heal bursts

--- playerbot/strategy/shaman/RestorationShamanStrategy.cpp
+++ playerbot/strategy/shaman/RestorationShamanStrategy.cpp
@@ -80,7 +80,8 @@ void RestorationShamanStrategy::InitCombatTriggers(std::list<TriggerNode*> &trig
 
     triggers.push_back(new TriggerNode(
         "medium aoe heal",
-        NextAction::array(0, new NextAction("chain heal", ACTION_MEDIUM_HEAL), NULL)));
+        NextAction::array(0, new NextAction("tidal force", ACTION_MEDIUM_HEAL + 1),
+            new NextAction("chain heal", ACTION_MEDIUM_HEAL), NULL)));
 
     triggers.push_back(new TriggerNode(
         "medium health",
